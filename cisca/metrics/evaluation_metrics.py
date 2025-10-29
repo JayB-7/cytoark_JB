7e4050d04ddf143e17179f6cb2a258624d2d1e3f
Jayanti Bhattacharya <74552597+JayB-7@users.noreply.github.com>
2025-10-29 19:29:41 +0530

    Update evaluation_metrics.py

--- cisca/metrics/evaluation_metrics.py
+++ cisca/metrics/evaluation_metrics.py
@@ -186,6 +186,127 @@ def get_multi_pq_info(true, pred, nr_classes=6, match_iou=0.5):
 
     return pq
 
+'''
+def get_pq(true, pred, match_iou=0.5):  # , remap=True
+    """Get the panoptic quality result.
+
+    Fast computation requires instance IDs are in contiguous orderding i.e [1, 2, 3, 4]
+    not [2, 3, 6, 10]. Please call `remap_label` beforehand. Here, the `by_size` flag
+    has no effect on the result.
+
+    Args:
+        true (ndarray): HxW ground truth instance segmentation map
+        pred (ndarray): HxW predicted instance segmentation map
+        match_iou (float): IoU threshold level to determine the pairing between
+            GT instances `p` and prediction instances `g`. `p` and `g` is a pair
+            if IoU > `match_iou`. However, pair of `p` and `g` must be unique
+            (1 prediction instance to 1 GT instance mapping). If `match_iou` < 0.5,
+            Munkres assignment (solving minimum weight matching in bipartite graphs)
+            is caculated to find the maximal amount of unique pairing. If
+            `match_iou` >= 0.5, all IoU(p,g) > 0.5 pairing is proven to be unique and
+            the number of pairs is also maximal.
+        remap (bool): whether to ensure contiguous ordering of instances.
+
+    Returns:
+        [dq, sq, pq]: measurement statistic
+
+        [paired_true, paired_pred, unpaired_true, unpaired_pred]:
+                      pairing information to perform measurement
+
+        paired_iou.sum(): sum of IoU within true positive predictions
+
+    """
+    assert match_iou >= 0.0, "Cant' be negative"
+    # ensure instance maps are contiguous
+    # if remap:
+    #     pred = skimage.segmentation.relabel_sequential(pred)[0]
+    #     true = skimage.segmentation.relabel_sequential(true)[0]
+
+    true = np.copy(true)
+    pred = np.copy(pred)
+    true = true.astype("int32")
+    pred = pred.astype("int32")
+    true_id_list = list(np.unique(true))
+    pred_id_list = list(np.unique(pred))
+    # prefill with value
+    pairwise_iou = np.zeros([len(true_id_list), len(pred_id_list)], dtype=np.float64)
+
+    # caching pairwise iou
+    for true_id in true_id_list[1:]:  # 0-th is background
+        t_mask_lab = true == true_id
+        rmin1, rmax1, cmin1, cmax1 = get_bounding_box(t_mask_lab)
+        t_mask_crop = t_mask_lab[rmin1:rmax1, cmin1:cmax1]
+        t_mask_crop = t_mask_crop.astype("int")
+        p_mask_crop = pred[rmin1:rmax1, cmin1:cmax1]
+        pred_true_overlap = p_mask_crop[t_mask_crop > 0]
+        pred_true_overlap_id = np.unique(pred_true_overlap)
+        pred_true_overlap_id = list(pred_true_overlap_id)
+        for pred_id in pred_true_overlap_id:
+            if pred_id == 0:  # ignore
+                continue  # overlaping background
+            p_mask_lab = pred == pred_id
+            p_mask_lab = p_mask_lab.astype("int")
+
+            # crop region to speed up computation
+            rmin2, rmax2, cmin2, cmax2 = get_bounding_box(p_mask_lab)
+            rmin = min(rmin1, rmin2)
+            rmax = max(rmax1, rmax2)
+            cmin = min(cmin1, cmin2)
+            cmax = max(cmax1, cmax2)
+            t_mask_crop2 = t_mask_lab[rmin:rmax, cmin:cmax]
+            p_mask_crop2 = p_mask_lab[rmin:rmax, cmin:cmax]
+
+            total = (t_mask_crop2 + p_mask_crop2).sum()
+            inter = (t_mask_crop2 * p_mask_crop2).sum()
+            iou = inter / (total - inter)
+            pairwise_iou[true_id - 1, pred_id - 1] = iou
+
+    if match_iou >= 0.5:
+        paired_iou = pairwise_iou[pairwise_iou > match_iou]
+        pairwise_iou[pairwise_iou <= match_iou] = 0.0
+        paired_true, paired_pred = np.nonzero(pairwise_iou)
+        paired_iou = pairwise_iou[paired_true, paired_pred]
+        paired_true += 1  # index is instance id - 1
+        paired_pred += 1  # hence return back to original
+    else:  # * Exhaustive maximal unique pairing
+        #### Munkres pairing with scipy library
+        # the algorithm return (row indices, matched column indices)
+        # if there is multiple same cost in a row, index of first occurence
+        # is return, thus the unique pairing is ensure
+        # inverse pair to get high IoU as minimum
+        paired_true, paired_pred = linear_sum_assignment(-pairwise_iou)
+        ### extract the paired cost and remove invalid pair
+        paired_iou = pairwise_iou[paired_true, paired_pred]
+
+        # now select those above threshold level
+        # paired with iou = 0.0 i.e no intersection => FP or FN
+        paired_true = list(paired_true[paired_iou > match_iou] + 1)
+        paired_pred = list(paired_pred[paired_iou > match_iou] + 1)
+        paired_iou = paired_iou[paired_iou > match_iou]
+
+    # get the actual FP and FN
+    unpaired_true = [idx for idx in true_id_list[1:] if idx not in paired_true]
+    unpaired_pred = [idx for idx in pred_id_list[1:] if idx not in paired_pred]
+    # print(paired_iou.shape, paired_true.shape, len(unpaired_true), len(unpaired_pred))
+
+    #
+    tp = len(paired_true)
+    fp = len(unpaired_pred)
+    fn = len(unpaired_true)
+    # get the F1-score i.e DQ
+    dq = tp / ((tp + 0.5 * fp + 0.5 * fn) + 1.0e-6)
+    # get the SQ, no paired has 0 iou so not impact
+    sq = paired_iou.sum() / (tp + 1.0e-6)
+
+    return (
+        [dq, sq, dq * sq],
+        [tp, fp, fn],
+        paired_iou.sum(),
+        [paired_true, unpaired_pred, unpaired_true],
+    )
+'''
+
+
 
 def get_pq(true, pred, match_iou=0.5):  # , remap=True
     """Get the panoptic quality result.
@@ -304,6 +425,7 @@ def get_pq(true, pred, match_iou=0.5):  # , remap=True
         paired_iou.sum(),
         [paired_true, unpaired_pred, unpaired_true],
     )
+
 
 
 ################################
